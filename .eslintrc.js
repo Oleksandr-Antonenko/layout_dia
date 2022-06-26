7f69470aa517646d88a9a0ba8cf70b46e0baf3c8
Antonenko-AA <alexandr.antonenko.fr@gmail.com>
2022-06-26 18:22:48 +0300

    make build

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: '@mate-academy/eslint-config',
-  parser: "babel-eslint",
-  parserOptions: {
-    sourceType: "module",
-    allowImportExportEverywhere: true
-  },
-};
